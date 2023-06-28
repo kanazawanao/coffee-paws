2d3d67e1631f732337dfbbe8424b6e99a646789c
kanazawanao <naomiti3333@gmail.com>
2023-06-29 00:21:06 +0900

    prettier設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "prettier"
     ],
     "overrides": [
         {
@@ -29,11 +30,18 @@ module.exports = {
     "plugins": [
         "@typescript-eslint",
         "react",
-        "react-hooks"
+        "react-hooks",
+        "unused-imports"
     ],
     "rules": {
         'react/react-in-jsx-scope': 'off',
         "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
+        "react-hooks/exhaustive-deps": "warn",
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+            "warn",
+            { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+        ]
     }
 }
