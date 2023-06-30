19df3e6cbf64b7aa1de19204a68a7f38bda56b5a
kanazawanao <naomiti3333@gmail.com>
2023-07-01 01:01:07 +0900

    envの設定方法見直し

---
+++ src/app/env.ts
@@ -0,0 +1,12 @@
+export const VITE_FIREBASE_API_KEY =
+  (import.meta.env.VITE_FIREBASE_API_KEY as string) || '';
+export const VITE_FIREBASE_AUTH_DOMAIN =
+  (import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string) || '';
+export const VITE_FIREBASE_PROJECT_ID =
+  (import.meta.env.VITE_FIREBASE_PROJECT_ID as string) || '';
+export const VITE_FIREBASE_STORAGE_BUCKET =
+  (import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string) || '';
+export const VITE_FIREBASE_MESSAGING_SENDER_ID =
+  (import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string) || '';
+export const VITE_FIREBASE_APP_ID =
+  (import.meta.env.VITE_FIREBASE_APP_ID as string) || '';
--- src/infrastructure/firebase.ts
+++ src/infrastructure/firebase.ts
@@ -1,13 +1,21 @@
+import {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID,
+} from 'app/env';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
