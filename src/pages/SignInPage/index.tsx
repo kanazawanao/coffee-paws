eb16ea022e6c00c0a091d21469cbf8c06b1bf4c2
kanazawanao <naomiti3333@gmail.com>
2023-08-11 01:06:23 +0900

    デザイン修正、ログイン済みならtopへ　

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -2,12 +2,22 @@ import { useAuthApi } from 'api/hooks/AuthApi';
 import FormContent from './FormContent';
 
 import styles from './style.module.scss';
+import { useCoffeePawsNavigate } from 'routes/hook';
+import React from 'react';
 
 export default function SignInPage() {
-  const { signIn } = useAuthApi();
+  const { goToTopPage } = useCoffeePawsNavigate();
+  const { signIn, user } = useAuthApi();
+
+  React.useEffect(() => {
+    if (user) {
+      goToTopPage();
+    }
+  }, [goToTopPage, user]);
+
   return (
     <div className={styles.container}>
-      <div>サインイン</div>
+      <div className={styles.title}>サインイン</div>
       <FormContent onSubmit={signIn} />
     </div>
   );
--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,12 +1,23 @@
 import { useAuthApi } from 'api/hooks/AuthApi';
 import FormContent from './FormContent';
+import styles from './style.module.scss';
+import { useCoffeePawsNavigate } from 'routes/hook';
+import React from 'react';
 
 export default function SignUpPage() {
-  const { signUp } = useAuthApi();
+  const { goToTopPage } = useCoffeePawsNavigate();
+  const { signUp, user } = useAuthApi();
+
+  React.useEffect(() => {
+    if (user) {
+      goToTopPage();
+    }
+  }, [goToTopPage, user]);
+
   return (
-    <>
-      <div>会員登録</div>
+    <div className={styles.container}>
+      <div className={styles.title}>会員登録</div>
       <FormContent onSubmit={signUp} />
-    </>
+    </div>
   );
 }
